16a380527cd5f89f7ea16bfc5f56745b7827b4ec
FilthyPail <45215695+FilthyPail@users.noreply.github.com>
2018-11-20 17:22:59 -0500

    Create Laptop-Wizard.sh

---
+++ Laptop-Wizard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Created by FilthyPail
+# Script created to help out with your laptop's configuration
+
+scriptVersion=1.0
+TruescriptVersion=1.0.0
+InternetTestURL="google.com"
+KextBoxURL="https://raw.githubusercontent.com/FilthyPail/Laptop-Wizard/master/KextBox.zip"
+KextBox=/Downloads/KextBox
+
+#Begin
+
